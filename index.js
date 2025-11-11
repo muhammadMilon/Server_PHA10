eaf85b27653c14ac30dc0417e63d890ad9cfbf62
Muhammad Milon <mmilon82814@gmail.com>
2025-11-11 17:11:09 +0600

    Express server successfully started — added "/" and "/users" endpoints

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+const users = [
+  { id: 1, name: 'Alice' },
+  { id: 2, name: 'Bob' },
+  { id: 3, name: 'Charlie' }
+]
+
+app.get('/users', (req, res) => {
+  res.json(users);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
